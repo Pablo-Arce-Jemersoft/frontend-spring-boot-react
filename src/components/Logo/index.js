65a1e082574eefc9d671e0bf760406b657a06ced
Pablo-Arce-Jemersoft <parce@jemersoft.com>
2022-10-21 14:27:59 -0300

    first commit

---
+++ src/components/Logo/index.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+// material-ui
+// eslint-disable-next-line no-unused-vars
+import { ButtonBase } from '@mui/material';
+
+// project import
+// eslint-disable-next-line no-unused-vars
+import Logo from './Logo';
+import config from 'config';
+
+// ==============================|| MAIN LOGO ||============================== //
+
+const LogoSection = ({ sx, to }) => (
+    <ButtonBase disableRipple component={Link} to={!to ? config.defaultPath : to} sx={sx}>
+        <Logo />
+    </ButtonBase>
+);
+
+LogoSection.propTypes = {
+    sx: PropTypes.object,
+    to: PropTypes.string
+};
+
+export default LogoSection;
