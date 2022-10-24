918ea0a3e0ac85f0346adbe5a2bd0de61a840f32
Pablo-Arce-Jemersoft <parce@jemersoft.com>
2022-10-24 09:21:40 -0300

    refactor: se eliminan archivos que no se utilizan

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -7,14 +7,11 @@ import MainLayout from 'layout/MainLayout';
 // render - dashboard
 const DashboardDefault = Loadable(lazy(() => import('pages/dashboard')));
 
-// render - sample page
-const SamplePage = Loadable(lazy(() => import('pages/extra-pages/SamplePage')));
+// render - other page
+// const OtherPage = Loadable(lazy(() => import('pages/other-pages/OtherPage')));
 
 // render - utilities
-const Typography = Loadable(lazy(() => import('pages/components-overview/Typography')));
-const Color = Loadable(lazy(() => import('pages/components-overview/Color')));
-const Shadow = Loadable(lazy(() => import('pages/components-overview/Shadow')));
-const AntIcons = Loadable(lazy(() => import('pages/components-overview/AntIcons')));
+// const Utility = Loadable(lazy(() => import('pages/components-overview/Utility')));
 
 // ==============================|| MAIN ROUTING ||============================== //
 
@@ -26,10 +23,10 @@ const MainRoutes = {
             path: '/',
             element: <DashboardDefault />
         },
-        {
-            path: 'color',
-            element: <Color />
-        },
+        // {
+        //     path: 'newPath',
+        //     element: <NewComponent />
+        // },
         {
             path: 'dashboard',
             children: [
@@ -38,22 +35,6 @@ const MainRoutes = {
                     element: <DashboardDefault />
                 }
             ]
-        },
-        {
-            path: 'sample-page',
-            element: <SamplePage />
-        },
-        {
-            path: 'shadow',
-            element: <Shadow />
-        },
-        {
-            path: 'typography',
-            element: <Typography />
-        },
-        {
-            path: 'icons/ant',
-            element: <AntIcons />
         }
     ]
 };
